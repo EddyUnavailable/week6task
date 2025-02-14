9ecd034d449850de9e1bb88d6bf162a665f0680c
eddy-sullivan <fleatoad@hotmail.com>
2025-02-14 12:56:23 +0000

    update2

--- cookierClickerII/components/Shop.jsx
+++ cookierClickerII/components/Shop.jsx
@@ -1,30 +1,32 @@
-import {useEffect, useState} from "react"
-import "../src/App.css"
-import Autoclicker from "./Autoclicker"
+import { useEffect, useState } from "react";
+import "../src/App.css";
+import Autoclicker from "./Autoclicker";
 
 export default function Shop() {
-    console.log('running shop')
-    const [upgrades, setUpgrades] = useState([])
+  console.log("running shop");
+  const [upgrades, setUpgrades] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const res = await fetch(`https://cookie-upgrade-api.vercel.app/api/upgrades`)
-            const data = await res.json()
-            setUpgrades(data)
-        }
-        fetchData()
-    },[])
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        `https://cookie-upgrade-api.vercel.app/api/upgrades`
+      );
+      const data = await res.json();
+      setUpgrades(data);
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <section>
-            {upgrades.map((upgrade) => (
-                <div key={upgrade.id} class = "shopRows" >
-                    <p>{upgrade.name}</p>
-                    <p>{upgrade.cost}</p>
-                    <p>{upgrade.increase}</p>
-                    <button>Buy</button>
-                </div>
-            ))}
-        </section>
-    )
+  return (
+    <section>
+      {upgrades.map((upgrade) => (
+        <div key={upgrade.id} class="shopRows">
+          <p class="shop_boxes">{upgrade.name}</p>
+          <p class="shop_boxes">{upgrade.cost}</p>
+          <p class="shop_boxes">{upgrade.increase}</p>
+          <button>Buy</button>
+        </div>
+      ))}
+    </section>
+  );
 }
--- cookierClickerII/src/App.jsx
+++ cookierClickerII/src/App.jsx
@@ -1,50 +1,53 @@
 import React, { useEffect, useState } from "react";
-import "./App.css"
+import "./App.css";
 // import Api_get from "../components/Api_get";
 import Shop from "../components/Shop";
 
 // const cookiesStored = localStorage.getItem(Cookies)
 
 export default function App() {
-    const [cookies, setCookies] = useState(100 || cookiesStored)
+  const [cookies, setCookies] = useState(null);
 
-    useEffect(() => {
-      const data = localStorage.getItem("cookie_value");
-      if ( data !== null ) setCookies(JSON.parse(data))
-      }, [])
+  useEffect(() => {
+    const data = localStorage.getItem("cookie_value");
+    if (data) {
+      setCookies(JSON.parse(data));
+    } else {
+      setCookies(100);
+    }
+  }, []);
 
-    // useEffect(() => {
-    //   const data = localStorage.getItem("cookie_value");
-    //   if (data) setCookies(JSON.parse(data))
-    //   }, []) 
+  useEffect(() => {
+    if (cookies) {
+      localStorage.setItem("cookie_value", JSON.stringify(cookies));
+    }
+  }, [cookies]);
 
-    useEffect(() => {
-      localStorage.setItem("cookie_value",JSON.stringify(cookies))
-    }, [cookies])
-      
-    useEffect(() => {
-      const id = setInterval(() => {
-        setCookies((prevCookies) => prevCookies + 1)
-    }, 1000)
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCookies((prevCookies) => prevCookies + 1);
+    }, 1000);
 
     return () => {
-      clearInterval(id)
-    }
-  }, [])
-
+      clearInterval(id);
+    };
+  }, []);
 
   return (
-    <section class = "mainContainer">
-      <div class = "firstDiv">
-        <p class = "display" >{cookies}</p>
-              
+    <section class="mainContainer">
+      <div class="firstDiv">
+        <img
+          src="./src/600.jpg"
+          width={100}
+          alt="The world"
+          onClick={() => setCookies(cookies + 1)}
+        />
       </div>
-      <div class = "firstDiv">
-        <img src="./src/600.jpg" width={100}   alt="The world" onClick={() => setCookies(cookies + 1)} />
+      <div class="firstDiv">
+        <p class="display">{cookies}</p>
       </div>
-      <div class = "firstDiv">
-      </div>
-        <Shop />
+      <div class="firstDiv"></div>
+      <Shop />
     </section>
   );
 }
